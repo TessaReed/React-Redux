2a8f07c7d199351187e92dceec948cfa8ca14ae1
Tessa <Tessa>
2018-01-16 09:57:43 +1100

    update search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,37 @@
+//purpose of this component, type into imput & make api request
+import React, { Component } from 'react';
+// curly braces means import reace and pull off property component as a variable.
+
+// create a function that returns an input
+// plain JS functional component
+
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+//class component is used when we want a component to be
+//aware of itself. Component needs to speak to application.
+// use ES6 class.
+
+// define a new class & give it access to all of functionality that react.Component has.
+class SearchBar extends Component {
+  render () {
+    return <input onChange={event => console.log(event.target.value)}
+    />;
+  }
+}
+  //whenever user interacts you have a change event.
+
+
+
+  //event handler - always called with an event object.
+  // event.target.value - get access to input
+  // use arrow function to combact code
+  {/* onInputChange(event) {
+    console.log(event.target.value);
+  } */}
+
+}
+
+export default SearchBar;
+//this is so any file that imports searchbar will get searchbarcomponent
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 //create a new component. This component should produce some html.
 // put the component HTML and put it on page. In the DOM.
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar.js';
  //declare a variable to hold Youtube API key
- const API_KEY = 'AIzaSyC80D-KKhajH2HVkaDYiJqt7_7UwNDdh5c';
+const API_KEY = 'AIzaSyC80D-KKhajH2HVkaDYiJqt7_7UwNDdh5c';
+
+//you have to create an app component and render components inside.
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
